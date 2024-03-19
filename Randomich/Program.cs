727637f73964c35a286b161bad0d46c332f40fa5
islamkagarmanov47172@gmail.com <islamkagarmanov47172@gmail.com>
2024-03-19 23:31:02 +0300

    Добавьте файлы проекта.

---
+++ Randomich/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class randomich
+{
+    static void Main()
+        
+    {
+        Random rnd = new Random();
+
+        int ocenca = rnd.Next(2, 6);
+        if ( ocenca == 2 ) 
+        {
+            Console.WriteLine("Неуд");
+        }
+        if (ocenca == 3)
+        {
+            Console.WriteLine("Удовл");
+        }
+        if (ocenca == 4)
+        {
+            Console.WriteLine("Хор");
+        }
+        if (ocenca == 5)
+        {
+            Console.WriteLine("Отл");
+        }
+    }
+    
+}
+
